6adbc46b932f2e54fda6ba3c4875dfefd9daaced
Harinder05 <harinder1998@live.it>
2023-09-14 19:06:27 +0100

    Added server side and client side validation for category

--- MVCProject/Controllers/CategoryController.cs
+++ MVCProject/Controllers/CategoryController.cs
@@ -24,9 +24,17 @@ namespace MVCProject.Controllers
         [HttpPost]
         public IActionResult Create(Category obj)
         {
-            _db.Categories.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index","Category"); // Redirect to action and controller
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", " The Display Order cannot be same as Name");
+            }
+            if(ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+            }
+            return View();
         }
     }
 }
--- MVCProject/Models/Category.cs
+++ MVCProject/Models/Category.cs
@@ -7,7 +7,9 @@ namespace MVCProject.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [MaxLength(40)]
         public string Name { get; set; }
+        [Range(1, 100)]
         public int DisplayOrder { get; set; }
     }
 }
