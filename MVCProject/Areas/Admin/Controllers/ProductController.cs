13ac548706d93cfe64ef46f4f326ebaa8dd44874
Harinder05 <harinder1998@live.it>
2023-09-26 22:36:35 +0100

    Added ability to register, login and added roles of admin and customer

--- MVCProject/Areas/Admin/Controllers/ProductController.cs
+++ MVCProject/Areas/Admin/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using MVCProject.DataAccess.Repository.IRepository;
 using MVCProject.Models;
@@ -6,6 +7,7 @@ using MVCProject.Models;
 namespace MVCProject.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles = "Admin")]
     public class ProductController : Controller
     {
         private readonly IProductRepository _productRepo;
